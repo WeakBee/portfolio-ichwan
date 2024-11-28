0336780afc4ba60df83c88674b2252c13bd7781b
ezypolis.ichwanulmuslim <ichwanul.muslim@ezypolis.com>
2024-11-28 09:41:39 +0700

    Update 4

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 for (let i = 1; i <= 6; i++){
     $( `.project-${i} .pic-indicator `).on( "click", function() {
         $(`.project-${i} .big-picture`).attr("src", $(this).attr("src"));
+        $(`.project-${i} .big-link`).attr("href", $(this).attr("src"));
     });
     
 }
